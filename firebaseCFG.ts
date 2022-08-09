ece438c197862ef4cb4255a18e6a910bfbb602f0
pedropaulowski <ixf34rxi@gmail.com>
2022-08-09 12:59:25 -0300

    escondendo variaveis da API do firestore, as anteriores já não existem mais

---
+++ firebaseCFG.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDmaqQTSA_uqf7iNC8rz_DLpoXMLsMoZ7w",
+    authDomain: "chess-multiplayer-f8414.firebaseapp.com",
+    projectId: "chess-multiplayer-f8414",
+    storageBucket: "chess-multiplayer-f8414.appspot.com",
+    messagingSenderId: "1089686293373",
+    appId: "1:1089686293373:web:f30d52bb204d20eea4da14",
+    measurementId: "G-NJQ4S3ENGQ"
+};
