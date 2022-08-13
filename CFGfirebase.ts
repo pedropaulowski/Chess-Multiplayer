ef8323b5759fe5797658a2dff47fbf6846940d0e
pedropaulowski <ixf34rxi@gmail.com>
2022-08-13 17:26:56 -0300

    update firebase

--- CFGfirebase.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDny_9uI3HzlklvpbhBJ1yo6BRbgRn81oY",
-    authDomain: "chess-multiplayer-35411.firebaseapp.com",
-    projectId: "chess-multiplayer-35411",
-    storageBucket: "chess-multiplayer-35411.appspot.com",
-    messagingSenderId: "238550826038",
-    appId: "1:238550826038:web:704b1480fdccbd843a6c97",
-    measurementId: "G-KTFD167QPS"
-};
